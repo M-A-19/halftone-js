1ac54b3126bc9359dad46621b30618beab62d943
M-A-19 <matt@enhancedcds.com>
2019-02-11 11:25:11 +0000

    Update angular-halftone.js
    Added the MIT license

--- angular/angular-halftone.js
+++ angular/angular-halftone.js
@@ -1,10 +1,30 @@
 /**
- * Halftone methods migrated from AS3 as an Angular Directive.
+ * Halftone methods ported from AS3 as an Angular Directive.
  *
  * You who turn the wheel and look to windward,
  * Consider Phlebas, who was once handsome and tall as you.
  *
- * @copyright Pez 2015
+ * @author Matthew Pearson <matt@enhancedcds.com>
+ * @license
+ * Copyright (c) 2015 Example Corporation Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 var app = angular.module("Halftone", []);
 
